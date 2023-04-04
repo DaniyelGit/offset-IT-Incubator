7f67c976a0d5c938a8811da22f22d0cab1fa68f6
Daniyel Tkachuk <daniyel_github-1@mail.ru>
2023-04-04 17:32:13 +0300

    refactor code

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,79 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
-import {CounterV1} from "./Counters/CounterV1";
+import {getDataState, saveDataState} from "./localStorage/localStorage";
+import {Settings} from "./components/Counter/Settings/Settings";
+import {Counter} from "./components/Counter/Counter/Counter";
 
 
-
+export type StateType = {
+   counterValue: number
+   startValue: number
+   maxValue: number
+   clueText: null | 'enter values and press set'
+}
 
 function App() {
+// ----- Counter -------
+   const [state, setState] = React.useState<StateType>({
+      counterValue: 0,
+      startValue: 0,
+      maxValue: 0,
+      clueText: null,
+   })
+
+   const {startValue, maxValue} = state;
+
+
+   useEffect(() => {
+      setState(getDataState('counterValues', state));
+   }, [])
+
+   useEffect(() => {
+      saveDataState('counterValues', {maxValue, startValue});
+   }, [maxValue, startValue]);
+
+
+   const changeStartMaxValue = (key: string, value: number): void => {
+      setState(prevState => {
+         return {
+            ...prevState,
+            [key]: value,
+            counterValue: -1,
+            clueText: 'enter values and press set',
+         }
+      })
+   }
+
+   const resetCurrentValue = (value: number) => {
+      setState({...state, counterValue: value})
+   }
+
+   const incrCurrentValue = () => {
+      setState({
+         ...state, counterValue: state.counterValue + 1
+      });
+   }
+
+   const setValue = (startValue: number) => {
+      setState({
+         ...state,
+         counterValue: startValue,
+         clueText: null,
+      });
+   };
 
    return (
       <div className="App-wrapper">
-         <CounterV1/>
+         <Settings
+            state={state}
+            changeStartMaxValue={changeStartMaxValue}
+            setValue={setValue}
+         />
+         <Counter
+            state={state}
+            incrCurrentValue={incrCurrentValue}
+            resetCurrentValue={resetCurrentValue}
+         />
       </div>
    );
 }
