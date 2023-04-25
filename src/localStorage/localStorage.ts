838b8c3162dec17880175b4582af5935e48f4387
Daniyel Tkachuk <daniyel_github-1@mail.ru>
2023-04-26 00:15:26 +0300

    added local storage

--- src/localStorage/localStorage.ts
+++ src/localStorage/localStorage.ts
@@ -1,9 +1,35 @@
-export const saveDataState = <T>(key: string, data: T) => {
+import {RootStateType} from "../redux/store";
+
+export const loadState = () => {
+   try {
+      const serializedState = localStorage.getItem('stateCounter');
+      if (serializedState === null) {
+         return undefined;
+      }
+      return JSON.parse(serializedState);
+   }
+   catch (error) {
+      console.log(error)
+   }
+}
+
+export const saveState = (state: RootStateType) => {
+   try {
+      const serializedState = JSON.stringify(state);
+      localStorage.setItem('stateCounter', serializedState);
+   }
+   catch(error) {
+      console.log(error);
+   }
+}
+
+
+/*export const saveDataState = <T>(key: string, data: T) => {
    localStorage.setItem(key, JSON.stringify({
       ...data,
    }));
-};
-
+};*/
+/*
 export const getDataState = <T>(key: string, defaultState: T) => {
    const dataFromLS = localStorage.getItem(key);
    if (dataFromLS) {
@@ -13,4 +39,4 @@ export const getDataState = <T>(key: string, defaultState: T) => {
          ...dataParse,
       });
    }
-}
+}*/
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,5 +1,8 @@
 import {combineReducers, legacy_createStore} from "redux";
 import {counterReducer} from "./reducers/counterReducer";
+import {loadState, saveState} from "../localStorage/localStorage";
+import throttle from 'lodash.throttle';
+
 
 const rootStore = combineReducers({
    counter: counterReducer,
@@ -7,7 +10,14 @@ const rootStore = combineReducers({
 
 export type RootStateType = ReturnType<typeof rootStore>;
 
-export const store = legacy_createStore(rootStore);
+const persistedState = loadState();
+export const store = legacy_createStore(rootStore, persistedState);
+
+store.subscribe(throttle(() => {
+   saveState({
+      counter: store.getState().counter
+   });
+}, 1000));
 
 // @ts-ignore
 window.store = store;
